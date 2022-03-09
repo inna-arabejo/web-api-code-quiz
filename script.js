75e4f1b9af8bd81388f4495ae1cfc5f2cdd36ec9
Anne Saber <iarabejo3@gmail.com>
2022-03-08 21:56:24 -0600

    Added a seperate questions html, added functions to js file.

---
+++ script.js
@@ -0,0 +1,45 @@
+//home page with start button
+const startButton = document.getElementById("start-button")
+
+startButton.addEventListener("click", beginQuiz)
+
+
+
+
+//click on start button 
+function startQuiz(){
+  console.log("Started")
+  
+}
+
+
+
+
+//the timer starts and presents the questions
+function presentQuestion(){
+
+}
+
+
+
+
+//answers question by clicking on it
+function answerSelected(){
+
+}
+
+
+
+
+//prompts another question
+
+//answering the question incorrectly, reduces time by 10 seconds
+
+//time reaches zero or questions are all answered
+
+//game is over
+
+//save the score with initials
+
+//returns to home page
+
