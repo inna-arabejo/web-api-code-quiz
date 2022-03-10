523aa9489561118bee2798a6283d267a96908272
Anne Saber <iarabejo3@gmail.com>
2022-03-10 14:23:56 -0600

    Added structure to how the js file will run code. Removed questions html file.

--- script.js
+++ script.js
@@ -1,45 +1,162 @@
-//home page with start button
-const startButton = document.getElementById("start-button")
 
-startButton.addEventListener("click", beginQuiz)
+var quizContent = document.getElementById("#quiz-content")
+const startButton = document.getElementById("#start-button");
+const questionContainer = document.getElementById("#question-card");
+const questionEl = Array.from(document.querySelectorAll("#question-choices");
+var latestTime = document.querySelector("#latestTime");
+var wrap = document.querySelector("#wrap");
+
+var questionIdx = 0;
+var score = 0;
+
+var questionTypes = [
+  {
+    question: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+
+];
 
 
 
 
-//click on start button 
-function startQuiz(){
-  console.log("Started")
+startButton.addEventListener("click", function(){
+  if(timeLeft === 0){
+    var timeInterval = setInterval(function(){
+      latestTime.textContent = "Timer: " + timeLeft;
+      timeLeft--;
+      
+      
+      if (timeLeft <= 0){
+        clearInterval(timeInterval);
+        latestTime.textContent = "Done!"
+
+        //timer reaches 0
+        countdown();
+      }
+    }, 1000);
+  }
+  render(questionIdx);
+});
+
+
+function render(questionIdx){
+  quizContent.innerHTML = "";
+  questionContainer.innerHTML = "";
+  
+
+  for (let i = 0; i < questionType.length; i++){
+    let typeOfQuestion = questionTypes[questionIdx].question;
+    let typeOfChoices = questionTypes[questionIdx].choices;
+    quizContent.textContent = typeOfQuestion;
+  }
+
+  typeOfChoices.forEach(function (newChoices){
+    var listOfChoices = document.createElement
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+// first question is presented
+function questionStarts(){
+  resetState()
   
 }
 
 
 
-
-//the timer starts and presents the questions
-function presentQuestion(){
+//prompts another question
+function nextQuestion(question){
+  button.addEventListener("click", answerSelected)
 
 }
-
+  
 
 
 
 //answers question by clicking on it
-function answerSelected(){
+function answerSelected(e){
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonEl.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
 
 }
 
 
+//shows question is correct or incorrect
+function setStatusClass(element, correct){
+  clearStatusClass(element)
+  
+}
 
 
-//prompts another question
+function clearStatusClass(element){
+
+  
+}
+
+
 
 //answering the question incorrectly, reduces time by 10 seconds
+function timeReduced(){
+
+}
+
 
 //time reaches zero or questions are all answered
 
 //game is over
+function gameOver(){
+
+}
+
 
 //save the score with initials
+function saveScore(){
 
-//returns to home page
+}
+
+
+//Quiz resets and goes back to home page
+function resetState(){
+  
+}
 
