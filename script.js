7bec63f459023e2a087feaaff2c187908cae39b5
Anne Saber <iarabejo3@gmail.com>
2022-03-12 00:29:50 -0600

    Displays the next question by creating forEach loop that goes through the array.

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ var startBtn = document.querySelector("#start-button");
 var questionSection = document.querySelector(".questions");
 var choicesBtns = document.querySelectorAll(".choices");
 var questionH2 = document.querySelector(".question")
+var scoreDisplay = document.querySelector(".score");
+var countdownDisplay = document.querySelector("#countdown");
 
 
 var currentIndex = 0;
-
-
-
-
+var score = 0;
+var countdown = 60;
 
 
 
@@ -54,7 +54,7 @@ startBtn.addEventListener("click", function() {
   startSection.classList.remove("active");
   questionSection.classList.add("active");
 
-  
+  scoreDisplay.textContent = score;
 
   
   showQuestions();
@@ -63,6 +63,44 @@ startBtn.addEventListener("click", function() {
 function showQuestions() {
   questionH2.textContent = questions[currentIndex].question;
 
+  choicesBtns.forEach((btn, index) => {
+    btn.textContent = questions[currentIndex].choices[index];
+
+    btn.addEventListener("click", function() {
+
+        if (btn.textContent === questions[currentIndex].answer) {
+            score++;
+            scoreDisplay.textContent = score;
+            nextQuestion();
+            // console.log(score);
+        } else {
+            countdown -= 10;
+        }
+        
+    })
+
+  })
 
 
+}
+
+
+function nextQuestion() {
+
+  if (currentIndex === (questions.length - 1)) {
+      console.log("no more questions")
+      // insert function to stop quiz
+      askInitials();
+
+
+  } else {
+      currentIndex++;
+
+      questionH2.textContent = questions[currentIndex].question;
+
+      choicesBtns.forEach((btn, index) => {
+      btn.textContent = questions[currentIndex].choices[index];
+      })
+
+  }
 }
